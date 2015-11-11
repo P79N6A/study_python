8ecdbbfddf3364e4329b2146dd9e0ae762ad69fa
luxi <luxi@youku.com>
2015-11-12 02:19:39 +0800

    优化python代码

--- check_image.py
+++ check_image.py
@@ -8,38 +8,29 @@ import os;
 import time;
 import json;
 
-# 把list_a在list_b中去重，并返回最新的list_b
-def remove_same_file(list_a,list_b):
-    new_list = list_b
-    for x in list_a:
-        if x in list_b:
-            new_list.pop(list_b.index(x))
-    return new_list;
-
-# 开始检查图片
 def check_image_from_path(bundle_path):
     # property
-    if len(bundle_path) == 0:
-        bundle_path = "."
+    if len(bundle_path) == 0 or cmp(bundle_path, '.') == 0:
+        bundle_path = os.path.join(".")
 
     if not os.path.isdir(bundle_path):
-        print "改路径不是一个文件夹，请检查 :("; return;
+        print "Error:\n Path: %s, 该路径不是一个文件夹，请检查 :(" % bundle_path;
+        return;
 
     tail_2x, tail_3x = "@2x.png", "@3x.png"
     list_all = os.listdir(bundle_path)
     list_img_ext = ['bmp','jpeg','gif','psd','png','jpg']
-    list_2x_images, list_3x_images, list_images, list_dir, list_other = [], [], [], [], []
+    list_images, list_dir, list_other = [], [], [];
 
     for x in list_all:
         if cmp(x[-len(tail_2x):], tail_2x) == 0:
             for y in list_all:
                 if cmp(y[-len(tail_3x):], tail_3x) == 0 and \
                    cmp(x[:(len(x) - len(tail_2x))], y[:(len(y) - len(tail_3x))]) == 0 :
-                    list_2x_images.append(x)
-                    list_3x_images.append(y)
+                    list_images.append(x)
+                    list_images.append(y)
 
-    list_strange = remove_same_file(list_3x_images[:],
-                                    remove_same_file(list_2x_images[:], list_all[:]));
+    list_strange = list(set(list_all) - set(list_images))
 
     for x in list_strange[:]:
         file_path = os.path.join(bundle_path, x)
@@ -51,20 +42,22 @@ def check_image_from_path(bundle_path):
             list_strange.pop(list_strange.index(x))
 
     # ending
-    print "共检查文件%s个" % len(list_all)
-    print "其中包含%s个文件夹" % len(list_dir) + "=>", json.dumps(list_dir, indent=1)
-    print "其他类型的文件:", json.dumps(list_other, indent=1)
-    print "和可能异常的%s个图片=>" % len(list_strange), json.dumps(list_strange, indent=1)
+    print "1.检查路径:%s" % bundle_path
+    print "2.共检查文件%s个" % len(list_all)
+    print "3.其中包含%s个文件夹" % len(list_dir) + "=>", json.dumps(list_dir, indent=1)
+    print "4.其他类型的文件:", json.dumps(list_other, indent=1)
+    print "5.可能异常的%s个图片=>" % len(list_strange), json.dumps(list_strange, indent=1)
 
     return
-
 
 #go
 print "\n" + "=" * 20 + "检查完成" + "=" * 20
 
 start_time = time.time()
-check_image_from_path("/Users/luxi/Works/DIDI/iOS Project/ditravel/DICarpool/DICarpoolRes.bundle")
+check_image_from_path('/Users/Risy/Works/DIDI/iOS Project/ditravel/DICarpool/DICarpoolRes.bundle')
 end_time = time.time() - start_time
 
-print "-", ">", "程序运行耗时:%0.8f秒" % end_time
+print "\n->", "程序运行耗时:%0.8f秒" % end_time
 print "=" * 20 + "have fun" + "=" * 20
+
+raw_input('\nPress Enter to exit')
---
+++ xcode_bundle_checker.py
@@ -0,0 +1,11 @@
+#!usr/bin/env python
+
+local_path = '/Users/Risy/Works/DIDI/iOS Project/ditravel/DICarpool/DICarpoolRes.bundle'
+
+str1 = "str1"
+str2 = 'str2'
+str1 = 'str3'
+print str1,str2
+
+set_value = set('1234')
+print set_value
